9da2ac0f057b61ac5f066a15817051a72b7f2b14
Šárka Flídrová <sarka.flidrova@email.cz>
2024-04-27 10:56:31 +0200

    Přidáno zavírání burger menu

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,10 @@ import { Gallery } from '../components/Gallery/Gallery';
 import { Contact } from '../components/Contact/Contact';
 import { Footer } from '../components/Footer/Footer';
 
+const toggleMenu = () => {
+  document.querySelector(".rollout-nav").classList.toggle("nav-closed")
+}
+
 document.querySelector('#root').innerHTML = render(
   <div className="page">
     <Header />
@@ -21,6 +25,6 @@ document.querySelector('#root').innerHTML = render(
   </div>
 );
 
-document.querySelector(".nav-btn").addEventListener("click", () => {
-  document.querySelector(".rollout-nav").classList.toggle("nav-closed")
-})
+document.querySelector(".nav-btn").addEventListener("click", toggleMenu)
+
+document.querySelector(".rollout-nav").addEventListener("click", toggleMenu)
