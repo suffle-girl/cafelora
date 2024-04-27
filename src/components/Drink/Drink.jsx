44f1863064ebe8a4028eff2a08601e3e87ef4cd0
Šárka Flídrová <sarka.flidrova@email.cz>
2024-04-27 13:38:14 +0200

    Přidán ternární operátor do komponenty Drink

--- src/components/Drink/Drink.jsx
+++ src/components/Drink/Drink.jsx
@@ -1,29 +1,28 @@
-import { Layer } from "../Layer/Layer"
-import "./Drink.css"
+import { Layer } from "../Layer/Layer";
+import "./Drink.css";
 
-export const Drink =({id, name, ordered, image, layers})=>{
-
-  return(
+export const Drink = ({ id, name, ordered, image, layers }) => {
+  return (
     <div className="drink">
-    <div className="drink__product">
-      <div className="drink__cup">
-        <img src={image} />
+      <div className="drink__product">
+        <div className="drink__cup">
+          <img src={image} />
+        </div>
+        <div className="drink__info">
+          <h3>{name}</h3>
+          {layers.map((item) => {
+            return <Layer color={item.color} label={item.label} />;
+          })}
+        </div>
       </div>
-      <div className="drink__info">
-        <h3>{name}</h3>
-        {layers.map((item)=>{
-          return(
-            <Layer color={item.color} label={item.label}/>
-          )
-        })}
-      </div>
+      <form className="drink__controls">
+        <input type="hidden" className="order-id" value="0" />
+        {ordered ? (
+          <button className="order-btn order-btn--ordered">Zrušit</button>
+        ) : (
+          <button className="order-btn">Objednat</button>
+        )}
+      </form>
     </div>
-    <form className="drink__controls">
-      <input type="hidden" className="order-id" value="0" />
-      <button className="order-btn">
-        Objednat
-      </button>
-    </form>
-  </div>
-  )
-}
+  );
+};
