347355785fcb41090f6f25f58e4680cd33f0f748
Šárka Flídrová <sarka.flidrova@email.cz>
2024-04-27 14:42:59 +0200

    Přidána metoda PATCH

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,51 @@
-import { render } from '@czechitas/render';
-import '../global.css';
-import './index.css';
-import { Banner } from '../components/Banner/Banner';
-import { Menu } from '../components/Menu/Menu';
-import { Header } from '../components/Header/Header';
-import { Gallery } from '../components/Gallery/Gallery';
-import { Contact } from '../components/Contact/Contact';
-import { Footer } from '../components/Footer/Footer';
+import { render } from "@czechitas/render";
+import "../global.css";
+import "./index.css";
+import { Banner } from "../components/Banner/Banner";
+import { Menu } from "../components/Menu/Menu";
+import { Header } from "../components/Header/Header";
+import { Gallery } from "../components/Gallery/Gallery";
+import { Contact } from "../components/Contact/Contact";
+import { Footer } from "../components/Footer/Footer";
 
-const response = await fetch("http://localhost:4000/api/drinks")
-const data = await response.json()
-const drinkList = data.data
-console.log(drinkList)
+const response = await fetch("http://localhost:4000/api/drinks");
+const data = await response.json();
+const drinkList = data.data;
+console.log(drinkList);
 
 const toggleMenu = () => {
-  document.querySelector(".rollout-nav").classList.toggle("nav-closed")
-}
+  document.querySelector(".rollout-nav").classList.toggle("nav-closed");
+};
 
-const handleOrderButton = (event)=>{
-  event.preventDefault()
-  const id = event.target.dataset.id
-  console.log(id)
-}
+const handleOrderButton = async (event) => {
+  event.preventDefault();
+  const id = event.target.dataset.id;
 
-document.querySelector('#root').innerHTML = render(
+  const resp = await fetch(`http://localhost:4000/api/drinks/${id}`)
+  const data = await resp.json();
+  const isOrdered = data.data.ordered;
+
+  const response = await fetch(`http://localhost:4000/api/drinks/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify([{
+      op: "replace",
+      path: "/ordered",
+      value: !isOrdered,
+    }]),
+  });
+  window.location.reload();
+  console.log(response)
+};
+
+document.querySelector("#root").innerHTML = render(
   <div className="page">
     <Header />
     <main>
       <Banner />
-      <Menu drinks={drinkList}/>
+      <Menu drinks={drinkList} />
       <Gallery />
       <Contact />
     </main>
@@ -36,11 +53,10 @@ document.querySelector('#root').innerHTML = render(
   </div>
 );
 
-document.querySelector(".nav-btn").addEventListener("click", toggleMenu)
+document.querySelector(".nav-btn").addEventListener("click", toggleMenu);
 
-document.querySelector(".rollout-nav").addEventListener("click", toggleMenu)
+document.querySelector(".rollout-nav").addEventListener("click", toggleMenu);
 
-document.querySelectorAll(".drink__controls").forEach((orderBtn)=>{
-  orderBtn.addEventListener("submit", handleOrderButton)
-})
-
+document.querySelectorAll(".drink__controls").forEach((orderBtn) => {
+  orderBtn.addEventListener("submit", handleOrderButton);
+});
